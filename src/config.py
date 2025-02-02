7c89764d13211cb6f0aae03ee06157cdab376d3a
Mina <minaaydogmus@gmail.com>
2025-02-02 01:10:31 +0100

    Ajout dossier src

---
+++ src/config.py
@@ -0,0 +1,8 @@
+train_data_path = "C:/Users/Mina/Desktop/BUT 3 ANNEE/titanic/data/train.csv"
+test_data_path = "C:/Users/Mina/Desktop/BUT 3 ANNEE/titanic/data/test.csv"
+output_directory_path = "C:/Users/Mina/Desktop/BUT 3 ANNEE/titanic/Output/"
+
+train_features_path = output_directory_path + "train_features.csv"
+train_labels_path = output_directory_path + "train_labels.csv"
+test_features_path = output_directory_path + "test_features.csv"
+rf_model_path = output_directory_path + "rf_model.pkl"
