341876df8687749c887843108a92d17a28a602c7
minaayd <142050788+minaayd@users.noreply.github.com>
2025-02-02 18:36:38 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,3 +6,4 @@ train_features_path = output_directory_path + "train_features.csv"
 train_labels_path = output_directory_path + "train_labels.csv"
 test_features_path = output_directory_path + "test_features.csv"
 rf_model_path = output_directory_path + "rf_model.pkl"
+ 
